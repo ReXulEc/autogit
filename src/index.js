d61ae66309f120a579ff5af0ca7dd5ffe029f4e4
Mert Doğu <mertdoguonder@protonmail.com>
2023-07-13 00:42:33 +0300

    feat: add branch check && fix bugs

--- src/index.js
+++ src/index.js
@@ -1,32 +1,50 @@
 const fs = require('fs');
-const {pull} = require('../util/pull.js');
-const {createLog} = require('../util/createLog.js');
+const { pull } = require('../util/pull.js');
+const { createLog } = require('../util/createLog.js');
 require('dotenv').config()
 const fastify = require('fastify')();
 
 fastify.post('/webhook', async (request, reply) => {
   const payload = request.body;
-  console.log('Received webhook payload:', payload.ref);
-  if (payload.ref === 'refs/heads/main') {
-    createLog('WEBHOOK', 'Received push event to master branch');
-    pull(process.env.LOCAL_PATH);
+
+  const branchCheck = () => {
+    if (payload.ref.replace('refs/heads/', '') === process.env.BRANCH) {
+      return {success: true, branch: payload.ref.replace('refs/heads/', '')};
+    } else {
+      return false;
+    }
   }
+
+  if (payload?.commits) {
+    if (branchCheck().success === true) {
+      createLog('WEBHOOK', 'Received push event to ' + branchCheck().branch + ' branch');
+      pull(process.env.LOCAL_PATH)
+    }
+  };
   //console.log('Received webhook payload:', payload);
   reply.send({ received: true });
 });
+
+/**.than((result) => {
+      if (result.error === true) {
+        createLog('ERROR', result.output);
+      } else {
+        createLog('SUCCESS', result.output);
+      }
+    }); */
 
 const PORT = +process.env.PORT || 3000;
 
 fastify.listen(
   {
-      port: PORT,
-      host: '0.0.0.0',
+    port: PORT,
+    host: '0.0.0.0',
   },
   (err) => {
-      if (err) {
-          createLog('ERROR', err.message);
-      } else {
-          createLog('SERVER', `Server is running on port ${PORT}`);
-      }
+    if (err) {
+      createLog('ERROR', err.message);
+    } else {
+      createLog('SERVER', `Server is running on port ${PORT}`);
+    }
   },
 );
--- util/pull.js
+++ util/pull.js
@@ -1,11 +1,20 @@
 const execSync = require('child_process').execSync;
+const { exec, spawn } = require('node:child_process');
+const { createLog } = require('./createLog.js');
+
 
 const pull = (path) => 
   {
     // import { execSync } from 'child_process';  // replace ^ if using ES modules
     let location = `git -C ${path} pull`
-    const output = execSync(location, { encoding: 'utf-8' });  // the default is 'buffer'
-    console.log('Output was:\n', output);
+    //execSync(location, { encoding: 'utf-8' }) 
+    exec(location, (err, stdout, stderr) => {
+      if (stderr.includes('error:')) {
+        createLog('ERROR', "Error pulling from git");
+      } else {
+        createLog('SUCCESS', stdout);
+      }
+    });
   }
 
 module.exports = {pull}
