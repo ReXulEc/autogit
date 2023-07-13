627f4a31460dae8288e5360cd1d167943bd0844e
Mert Doğu <mertdoguonder@protonmail.com>
2023-07-13 11:58:00 +0300

    feat: add logs

--- util/createSecret.js
+++ util/createSecret.js
@@ -1,6 +1,13 @@
 const env = require('rapidenv')();
 const crypto = require('crypto');
+const { createLog } = require('./createLog.js');
 
-env.load();
-var token = crypto.randomBytes(64).toString('hex');
-env.setVariable("WEBHOOK_SECRET", token);
+const createSecret = () => {
+    env.load();
+    var token = crypto.randomBytes(64).toString('hex');
+    createLog('ERROR', 'WEBHOOK_SECRET is not valid.', true);
+    env.setVariable("WEBHOOK_SECRET", token);
+    createLog('INFO', 'New WEBHOOK_SECRET created. You can find it in .env file.', true);
+};
+
+module.exports = {createSecret};
