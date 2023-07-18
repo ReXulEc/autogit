f29c5ede4cc174372a0b7c1c17f83df4aa659ed2
Mert Doğu <mertdoguonder@protonmail.com>
2023-07-18 12:10:19 +0300

    feat: add git name

--- util/pull.js
+++ util/pull.js
@@ -4,17 +4,17 @@ const { createLog } = require('./createLog.js');
 const { runScript } = require('./runScript.js');
 
 
-const pull = (path) => 
+const pull = (path, git, scripts) => 
   {
     // import { execSync } from 'child_process';  // replace ^ if using ES modules
     let location = `git -C ${path} pull`
     //execSync(location, { encoding: 'utf-8' }) 
     exec(location, (err, stdout, stderr) => {
       if (stderr.includes('error:')) {
-        createLog('ERROR', "Error while pulling from git\n\n" + stderr, true);
+        createLog(git + ' | ERROR', "Error while pulling from git\n\n" + stderr, true);
       } else {
-        createLog('SUCCESS', stdout);
-        runScript();
+        createLog(git + ' | SUCCESS', stdout);
+        runScript(scripts, git);
 
       }
     });
