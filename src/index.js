7de3900272af99cd1700883d5eb3d2928f3eba8c
Mert Doğu <mertdoguonder@protonmail.com>
2023-07-18 12:09:21 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -10,15 +10,15 @@ const PORT = +process.env.PORT || 3000;
 const argv = require('minimist')(process.argv.slice(2));
 let startTime = process.hrtime();
 
-if(argv.devScript === "true") {
-  (async function() {
+if (argv.devScript === "true") {
+  (async function () {
     await ngrok.connect({
       addr: PORT, // port or network address, defaults to 80
       region: process.env.NGROK_REGION, // one of ngrok regions (us, eu, au, ap, sa, jp, in), defaults to us
-      onStatusChange: status => {createLog("NGROK STATUS", status.charAt(0).toUpperCase() + status.slice(1))}, // 'closed' - connection is lost, 'connected' - reconnected
+      onStatusChange: status => { createLog("NGROK STATUS", status.charAt(0).toUpperCase() + status.slice(1)) }, // 'closed' - connection is lost, 'connected' - reconnected
       onLogEvent: data => {
-        if(data.includes("url")) {
-          let url = data.split(" ")[data.split(" ").length -1].slice(4)
+        if (data.includes("url")) {
+          let url = data.split(" ")[data.split(" ").length - 1].slice(4)
           createLog("NGROK", `Ngrok is running on ${url}`)
         }
       },
@@ -26,38 +26,40 @@ if(argv.devScript === "true") {
   })();
 }
 
-
-fastify.post('/webhook', async (request, reply) => {
-    if (handleSignature(request, request.headers) === false) {
-      createLog('ERROR', 'Invalid signature');
-      reply.code(401).send({ received: false, error: 'Invalid signature.' });
-    }
-
-  const payload = request.body;
-  const branchCheck = () => {
-    if (payload.ref.replace('refs/heads/', '') === process.env.BRANCH) {
-      return {success: true, branch: payload.ref.replace('refs/heads/', '')};
-    } else {
-      return false;
-    }
-  }
-
-  if (payload?.commits) {
-    if (branchCheck().success === true) {
-      createLog('WEBHOOK', 'Received push event to ' + branchCheck().branch + ' branch');
-        await pull(process.env.LOCAL_PATH)
-    };
-  reply.send({ received: true });
-  }
-});
-
 fs.readFile(process.env.DATA_PATH, 'utf8', (err, content) => {
   if (err) {
     checkEnv(err, false);
   }
-  const jsonData = JSON.parse(content);
-  checkEnv(jsonData, true).then((output) => {
-    if(output === true) {
+  let config = JSON.parse(content);
+  fastify.post('/webhook', async (request, reply) => {
+    if (handleSignature(request) === false) {
+      createLog('ERROR', 'Invalid signature');
+      reply.code(401).send({ received: false, error: 'Invalid signature.' });
+    }
+
+    config.listen.forEach(async (repo) => {
+      if (repo.git === request.body.repository.full_name) {
+        const branchCheck = () => {
+          if (request.body.ref.replace('refs/heads/', '') === repo.branch) {
+            return { success: true, branch: request.body.ref.replace('refs/heads/', '') };
+          } else {
+            return false;
+          }
+        }
+
+        if (request.body?.commits) {
+          if (branchCheck().success === true) {
+            createLog(repo.git + ' | WEBHOOK', 'Received push event to ' + branchCheck().branch + ' branch');
+            await pull(repo.path, repo.git, repo.scripts);
+          };
+          reply.send({ received: true });
+        }
+      }
+    });
+  });
+
+  checkEnv(config, true).then((output) => {
+    if (output) {
       fastify.listen(
         {
           port: PORT,
@@ -68,9 +70,7 @@ fs.readFile(process.env.DATA_PATH, 'utf8', (err, content) => {
             createLog('ERROR', err.message);
           } else {
             createLog('SERVER', `Server is running on port ${PORT}`);
-            let endTime = process.hrtime(startTime);
-            let duration = endTime[0] * 1000 + endTime[1] / 1000000;
-            createLog('SERVER', `Server started in ${duration.toFixed(3)}ms`);
+            createLog('SERVER', `Server started in ${(process.hrtime(startTime)[0] * 1000 + process.hrtime(startTime)[1] / 1000000).toFixed(3)}ms`);
           }
         },
       );
