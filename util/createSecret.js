9e54c061dd0e45eb860f52188e97307a78df3fd8
Mert Doğu <mertdoguonder@protonmail.com>
2023-07-18 12:10:01 +0300

    refactor

--- util/createSecret.js
+++ util/createSecret.js
@@ -4,9 +4,8 @@ const { createLog } = require('./createLog.js');
 
 const createSecret = () => {
     env.load();
-    var token = crypto.randomBytes(64).toString('hex');
     createLog('ERROR', 'WEBHOOK_SECRET is not valid.', true);
-    env.setVariable("WEBHOOK_SECRET", token);
+    env.setVariable("WEBHOOK_SECRET", crypto.randomBytes(64).toString('hex'));
     createLog('INFO', 'New WEBHOOK_SECRET created. You can find it in .env file.', true);
 };
 
